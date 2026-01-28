12bae8a2b608ea178d8c50925b3cd4e78918a709
ANDOUR Ismail <andour.student@gmail.com>
2026-01-28 12:43:35 +0100

    Exercices and challenges

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -57,6 +57,7 @@ console.log(typeof(Usernumber));
 console.log(`Your number is : ${Usernumber}!`);
 while(Usernumber < 10) {
     let Usernumber = parseInt(prompt("Please enter a new number:"));
+    break;
 } //Q2 While the number is smaller than 10 continue asking the user for a new number.
 
 // 🌟 Exercise 4 : Building Management
@@ -121,4 +122,52 @@ for (let i = 0; i < names.length; i++) {
 }
 console.log(societyName); //Console.log the name of their secret society. The output should be “ABJKPS” 
    // ====== Daily Challenge 2
+   //Exercise 1 : Divisible by three
+   //Loop through the array above and determine whether or not each number is divisible by three.
+   let numbers = [123, 8409, 100053, 333333333, 7];
+   for (let i = 0; i < numbers.length; i++) {
+       if (numbers[i] % 3 === 0) {
+           console.log(`${numbers[i]} is divisible by three.`);
+       } else {
+           console.log(`${numbers[i]} is not divisible by three.`);
+       }
+   }
+   //Each time you loop console.log true or false.
+   function istrue(){
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 3 === 0) {
+           console.log(true);
+       } else {
+           console.log(false);
+       }
+     }
+}
+istrue();
+//Exercise 2 : Attendance
+let guestList = {
+  randy: "Germany",
+  karla: "France",
+  wendy: "Japan",
+  norman: "England",
+  sam: "Argentina"
+}
+//Prompt the student for their name.
+let Studentname = prompt("Enter your  first name :");
+console.log(`Student name is : ${Studentname}`);
+//If the name is in the object, console.log the name of the student and the country they come from.
+console.log(guestList[Studentname] ? `Student name is : ${Studentname} and the country they come from is : ${guestList[Studentname]}` : "Name not found in the guest list.");
+//If the name is not in the object, console.log: "Hi! I'm a guest."
+if (!guestList[Studentname]) {
+    console.log("Hi! I'm a guest.");
+}
+//Exercise 3 : Playing with numbers
+let age = [20,5,12,43,98,55];
+//Console.log the sum of all the numbers in the age array.
+let sum =0;
+for(let i =0; i < age.length ; i++){
+    sum += age[i];
+}
+console.log(sum);
+//Console.log the highest age in the array.
+console.log(Math.max(...age));
 // ====== Daily Challenge 3
