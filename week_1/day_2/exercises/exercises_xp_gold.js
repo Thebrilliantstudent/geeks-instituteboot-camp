a92c82624fe56a053633e933ef410e493b8f5ecc
ANDOUR Ismail <andour.student@gmail.com>
2026-02-03 10:31:57 +0100

    Exercices and challenges

--- week_1/day_2/exercises/exercises_xp_gold.js
+++ week_1/day_2/exercises/exercises_xp_gold.js
@@ -51,3 +51,4 @@ console.log(isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6)); //➞ false
 
 // ===== Exercise 5
 //Javascript code to color all diagonal table cells in red.
+// Script in the Html page : page2.html
